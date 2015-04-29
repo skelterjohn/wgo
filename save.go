9563347e8f1c212a8a1979f5697d0104cf185afe
John Asmuth <jasmuth@google.com>
2015-04-29 15:24:28 -0400

    Error for bad flags to 'wgo init'.

--- save.go
+++ save.go
@@ -87,6 +87,16 @@ func save(w *workspace) {
 	for destination, dir := range addonMapping {
 		addonArgs = append(addonArgs, "-a", destination+"="+dir)
 	}
+
+	ignoreDirs := []string{".git", ".hg", ".gocfg"}
+	for _, gopath := range w.gopaths {
+		ignoreDirs = append(ignoreDirs,
+			filepath.Join(gopath, "pkg"),
+			filepath.Join(gopath, "bin"))
+	}
+	ignoreDirsStr := strings.Join(ignoreDirs, string(filepath.ListSeparator))
+	os.Setenv("VENDOR_IGNORE_DIRS", ignoreDirsStr)
+
 	w.shellOutToVendor(
 		append([]string{"wgo", "vendor", "-s"}, addonArgs...))
 }
